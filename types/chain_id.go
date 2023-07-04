6234466c2bbe5f6790c7a2f40e10a800358dad10
Daniil Lashin <danil-lashin@yandex.ru>
2023-07-04 09:11:36 +0300

    WIP: adapt evmos to mineplex

--- types/chain_id.go
+++ types/chain_id.go
@@ -49,7 +49,7 @@ func IsValidChainID(chainID string) bool {
 // chain-id in *big.Int format. The function returns an error if the chain-id has an invalid format
 func ParseChainID(chainID string) (*big.Int, error) {
 	if strings.HasPrefix(chainID, "mineplex-testnet") {
-		return big.NewInt(1), nil
+		return big.NewInt(4156), nil
 	}
 
 	return big.NewInt(4157), nil
