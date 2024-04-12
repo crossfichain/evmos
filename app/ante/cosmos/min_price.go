568be51d084664720adaa9c9f62c12a20ecf653d
mineplexio <info@mineplex.io>
2024-04-12 10:02:56 +0300

    Fix fees

--- app/ante/cosmos/min_price.go
+++ app/ante/cosmos/min_price.go
@@ -60,7 +60,7 @@ func (mpd MinGasPriceDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate 
 		},
 		{
 			Denom:  "xfi",
-			Amount: minGasPrice.MulInt64(2),
+			Amount: minGasPrice.QuoInt64(100),
 		},
 	}
 
