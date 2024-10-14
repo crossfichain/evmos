b80f57dff969a6e53db9f4e2f417c92817d2e1e1
mineplexio <info@mineplex.io>
2024-10-14 20:47:32 +0300

    Fix SupportedAlgorithms

--- crypto/keyring/options.go
+++ crypto/keyring/options.go
@@ -17,6 +17,7 @@
 package keyring
 
 import (
+	cosmoshd "github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	cosmosLedger "github.com/cosmos/cosmos-sdk/crypto/ledger"
 	"github.com/cosmos/cosmos-sdk/crypto/types"
@@ -32,7 +33,7 @@ const AppName = "Ethereum"
 var (
 	// SupportedAlgorithms defines the list of signing algorithms used on Evmos:
 	//  - eth_secp256k1 (Ethereum)
-	SupportedAlgorithms = keyring.SigningAlgoList{hd.EthSecp256k1}
+	SupportedAlgorithms = keyring.SigningAlgoList{hd.EthSecp256k1, cosmoshd.Secp256k1}
 	// SupportedAlgorithmsLedger defines the list of signing algorithms used on Evmos for the Ledger device:
 	//  - secp256k1 (in order to comply with Cosmos SDK)
 	// The Ledger derivation function is responsible for all signing and address generation.
